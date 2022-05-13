2e9a27c83b4a05b8a4908aa3069a3e0f4c178eee
sebastiangancino <sjgancino97sd@gmail.com>
2022-05-13 16:46:00 -0500

    todo bien, iniciamos clase 18

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import '../styles/OrderItem.scss';
+import React, { useState } from 'react';
+import '@styles/ProductItem.scss';
+import addToCart from '@icons/bt_add_to_cart.svg';
 
-const OrderItem = () => {
+const ProductItem = () => {
+
+	const [cart, setCart] = useState([]);
+
+	const handleClick = () => {
+		setCart([]);
+	}
+  
+
 	return (
-		<div className="OrderItem">
-			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
-			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
-			<img src="./icons/icon_close.png" alt="close" />
+		<div className="ProductItem">
+			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<div className="product-info">
+				<div>
+					<p>$120,00</p>
+					<p>Bike</p>
+				</div>
+				<figure onClick={handleClick} >
+					<img src={addToCart} alt="" />
+				</figure>
+			</div>
 		</div>
 	);
 }
 
-export default OrderItem;
+export default ProductItem;
